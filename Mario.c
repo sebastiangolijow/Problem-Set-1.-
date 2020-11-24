3dab60c53f468872bf58c26550254891b4436fdd
sebastiangolijow <71946219+sebastiangolijow@users.noreply.github.com>
2020-11-24 12:18:20 -0300

    Create Mario.c

---
+++ Mario.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h> 
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n > 1 and n < 8);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
